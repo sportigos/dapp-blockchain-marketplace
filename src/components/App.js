fa6fcfd879365d682cc279c477955f85edad5cdd
LeonidAlexeyev <sportigo1120@gmail.com>
2021-06-10 05:10:37 +0300

    Updated add product on client-side

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import logo from '../logo.png';
 import './App.css';
 import Web3 from 'web3'
 import Navbar from './Navbar'
+import Main from './Main'
 import Marketplace from '../abis/Marketplace.json'
 
 class App extends Component {
@@ -68,29 +69,12 @@ class App extends Component {
         <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
+          <main role="main" className="col-lg-12 d-flex">
+            { this.state.loading
+              ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+              : <Main createProduct={this.createProduct} />
+            }
+          </main>
           </div>
         </div>
       </div>
